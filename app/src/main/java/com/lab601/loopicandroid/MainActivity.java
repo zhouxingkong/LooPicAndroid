03a0ae542f33ecd4a1061da3d8c98b34fd820923
xingkong <508818018@qq.com>
2019-06-02 10:10:59 +0800

    全屏浏览

--- app/src/main/java/com/lab601/loopicandroid/MainActivity.java
+++ app/src/main/java/com/lab601/loopicandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lab601.loopicandroid;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
@@ -14,20 +17,27 @@ import com.github.chrisbanes.photoview.PhotoView;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import static android.R.attr.uiOptions;
+
 public class MainActivity extends AppCompatActivity {
     LooViewPager photoView;
 
+    int totalPages = 0; //总共图片数
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         photoView = (LooViewPager) findViewById(R.id.photo_view);
-        photoView.setBackgroundColor(R.color.black);
+        photoView.setBackgroundColor(Color.BLACK);
         photoView.setAdapter(new LooPagerAdapter());
         photoView.setOnClickListener((view) -> {
+            Log.d("xinkong", "view点击");
             int curr = photoView.getCurrentItem();
             photoView.setCurrentItem(curr + 1, true);
         });
+        fullScreen();
     }
 
     class LooPagerAdapter extends PagerAdapter {
@@ -67,6 +77,52 @@ public class MainActivity extends AppCompatActivity {
             return view == object;
         }
 
+    }
+
+    public void fullScreen() {
+
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        boolean isImmersiveModeEnabled = isImmersiveModeEnabled();
+        if (isImmersiveModeEnabled) {
+            Log.i("TEST", "Turning immersive mode mode off. ");
+        } else {
+            Log.i("TEST", "Turning immersive mode mode on.");
+        }
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
+    }
+
+    private boolean isImmersiveModeEnabled() {
+        return ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
     }
 
 }
