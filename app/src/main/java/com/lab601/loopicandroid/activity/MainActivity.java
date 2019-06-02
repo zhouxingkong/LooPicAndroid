7e5f78529a65450505307dd38af4099f522f73f9
xingkong <508818018@qq.com>
2019-06-02 17:57:01 +0800

    +音频播放

--- app/src/main/java/com/lab601/loopicandroid/activity/MainActivity.java
+++ app/src/main/java/com/lab601/loopicandroid/activity/MainActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.util.Log;
@@ -16,21 +18,29 @@ import android.widget.TextView;
 
 import com.github.chrisbanes.photoview.PhotoView;
 import com.lab601.loopicandroid.R;
+import com.lab601.loopicandroid.Utils;
 import com.lab601.loopicandroid.bean.OutputDesc;
 import com.lab601.loopicandroid.view.LooViewPager;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
     public static final String ROOT_PATH = "/sdcard/bb/output";
     public static final String PICTURE_PATH = ROOT_PATH + "/imgs";
+    public static final String SOUND_ROOT = "/sdcard/bb/sounds";
+
+    public static final int WHAT_PLAY_VIDEO = 10;
+
+
     LooViewPager photoView;
     TextView textView;
 
@@ -41,18 +51,61 @@ public class MainActivity extends BaseActivity {
 
     List<OutputDesc> outputDescs;
 
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {  //这个是发送过来的消息
+            switch (msg.what) {
+                case WHAT_PLAY_VIDEO: {
+                    playSound((String) msg.obj);
+                    break;
+                }
+            }
+        }
+    };
+
+    /**
+     * @param index
+     */
     public void onPageChanged(int index) {
         Log.d("xingkong", "浏览索引:" + index);
 
 
+
+
+        /*播放音频*/
+        String soundTag = outputDescs.get(index).getSoundTag();
+        String soundMode = outputDescs.get(index).getSoundMode();
+        //todo: 添加更高级的标签匹配机制
+        if (!soundTag.equals("#") && soundTag.length() > 0) {
+            soundTag = soundTag.replace("-", "/");
+            File soundDir = new File(SOUND_ROOT + "/" + soundTag);
+//            Log.d(TAG, "onPageChanged: 音频路径:"+SOUND_ROOT+"/"+soundTag);
+            final File[] soundFiles = soundDir.listFiles();
+            List<File> files = new ArrayList<File>(Arrays.asList(soundFiles));
+            if (files.size() > 1) {
+                Utils.shuffle(files);
+            }
+            if (files.size() > 0) {
+                playSound(files.get(0).getPath());
+//                handler.sendMessage(handler.obtainMessage(WHAT_PLAY_VIDEO,files.get(0).getPath()));    //发送初始化信息
+            }
+        } else {
+            mediaPlayer.stop();
+        }
+
+
+//        handler.sendMessage(handler.obtainMessage(WHAT_PLAY_VIDEO,files.get(0).getPath()));    //发送初始化信息
         /*显示文本*/
         String text = outputDescs.get(index).getText();
-        if (!text.equals("#")) {
+        if (text.equals("#")) {
             text = "";
         }
         textView.setText(text);
 
+
     }
+
 
     /**
      * 读取输入文件
@@ -91,6 +144,7 @@ public class MainActivity extends BaseActivity {
         readConfigFile(ROOT_PATH + "/index.txt");
 
         mediaPlayer = new MediaPlayer();    //音频播放器
+//        mediaPlayer = MediaPlayer.create(this,R.raw.sound);
 
         /*初始化view*/
         textView = (TextView) findViewById(R.id.loo_text);
@@ -116,19 +170,29 @@ public class MainActivity extends BaseActivity {
      * @param path
      */
     public void playSound(String path) {
+        Log.d("xingkong", "playSound: 路径:" + path);
         try {
+//            File file = new File(path);
+//            FileInputStream fis = new FileInputStream(file);
+//            mediaPlayer.reset();
+//            mediaPlayer.setDataSource(fis.getFD());
+            mediaPlayer.stop();
+            mediaPlayer = new MediaPlayer();
             mediaPlayer.setDataSource(path);
             mediaPlayer.prepare();
+            mediaPlayer.start();
         } catch (IOException e) {
             Log.e("xingkong", "playSound: 播放音频异常", e);
             e.printStackTrace();
         }
-        /*播放完成回调函数*/
-        mediaPlayer.setOnCompletionListener((mp -> {
+//        /*播放完成回调函数*/
+//        mediaPlayer.setOnCompletionListener((mp -> {
+//
+//        }));
 
-        }));
-        mediaPlayer.start();
     }
+
+
 
     class LooPagerAdapter extends PagerAdapter {
 
